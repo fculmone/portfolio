50a994dbbf43917d644ea61d1c734e87098d1f9e
Francesco Culmone <fculmone5@gmail.com>
2025-06-14 01:15:18 -0400

    changed paths to relative and changed config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/portfolio/",
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "aboutme/index.html",
+      },
+    },
+  },
+});
